2668a17e299b5cc32a93984a6d9a9a3921660145
Dina <dinaform@gmail.com>
2021-05-10 18:57:42 +0300

    Add project files.

---
+++ Controllers/QueriesController.cs
@@ -0,0 +1,441 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TheGStore.Models;
+
+namespace TheGStore.Controllers
+{
+    public class QueriesController : Controller
+    {
+        public IConfiguration Configuration { get; }
+
+        private const string S1_PATH = @"C:\Users\dinaf\OneDrive\Рабочий стол\BD.TheGStore-main\Queries\S1.sql";
+        private const string S2_PATH = @"C:\Users\dinaf\OneDrive\Рабочий стол\BD.TheGStore-main\Queries\S2.sql";
+        private const string S3_PATH = @"C:\Users\dinaf\OneDrive\Рабочий стол\BD.TheGStore-main\Queries\S3.sql";
+        private const string S4_PATH = @"C:\Users\dinaf\OneDrive\Рабочий стол\BD.TheGStore-main\Queries\S4.sql";
+        private const string S5_PATH = @"C:\Users\dinaf\OneDrive\Рабочий стол\BD.TheGStore-main\Queries\S5.sql";
+        private const string S6_PATH = @"C:\Users\dinaf\OneDrive\Рабочий стол\BD.TheGStore-main\Queries\S6.sql";
+
+        private const string A1_PATH = @"C:\Users\dinaf\OneDrive\Рабочий стол\BD.TheGStore-main\Queries\A1.sql";
+        private const string A2_PATH = @"C:\Users\dinaf\OneDrive\Рабочий стол\BD.TheGStore-main\Queries\A2.sql";
+        private const string A3_PATH = @"C:\Users\dinaf\OneDrive\Рабочий стол\BD.TheGStore-main\Queries\A3.sql";
+
+        private const string T1_PATH = @"C:\Users\dinaf\OneDrive\Рабочий стол\BD.TheGStore-main\Queries\T1.sql";
+        private const string T2_PATH = @"C:\Users\dinaf\OneDrive\Рабочий стол\BD.TheGStore-main\Queries\T2.sql";
+
+       
+        
+        private readonly TheGStoreDbContext _context;
+
+        public QueriesController(TheGStoreDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            Configuration = configuration;
+        }
+
+        public IActionResult Index(int errorCode)
+        {
+            var customers = _context.Customers.Select(c => c.FirstName).Distinct().ToList();
+            if (errorCode == 1)
+            {
+                ViewBag.ErrorFlag = 1;
+                ViewBag.PriceError = "Введіть коректну вартість";
+            }
+            if (errorCode == 2)
+            {
+                ViewBag.ErrorFlag = 2;
+                ViewBag.ProdNameError = "Поле необхідно заповнити";
+            }
+
+            var empty = new SelectList(new List<string> { "--Пусто--" });
+            var anyCusts = _context.Customers.Any();
+            var anyDevs = _context.Developers.Any();
+
+            ViewBag.DevIds = anyDevs ? new SelectList(_context.Developers, "Id", "Id") : empty;
+            ViewBag.DevNames = anyDevs ? new SelectList(_context.Developers, "FirstName", "FirstName") : empty;
+            ViewBag.CustNames = anyCusts ? new SelectList(customers) : empty;
+            ViewBag.CustEmails = anyCusts ? new SelectList(_context.Customers, "Email", "Email") : empty;
+            ViewBag.CustSurnames = anyCusts ? new SelectList(_context.Customers, "LastName", "LastName") : empty;
+            ViewBag.Countries = _context.Countries.Any() ? new SelectList(_context.Countries, "FirstName", "FirstName") : empty;
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SimpleQuery1(Query queryModel)
+        {
+            string query = System.IO.File.ReadAllText(S1_PATH);
+            query = query.Replace("P", "N\'" + queryModel.DevName + "\'");
+            query = query.Replace("\r\n", " ");
+            query = query.Replace('\t', ' ');
+
+            queryModel.QueryId = "S1";
+
+            using (var connection = new SqlConnection(Configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(query, connection))
+                {
+                    var result = command.ExecuteScalar();
+                    if (result != DBNull.Value)
+                    {
+                        queryModel.AvgPrice = Convert.ToDecimal(result);
+                    }
+                    else
+                    {
+                        queryModel.ErrorFlag = 1;
+                        queryModel.Error = Resourses.ERROR_AVG_PRICE;
+                    }
+                }
+                connection.Close();
+            }
+            return RedirectToAction("Result", queryModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SimpleQuery2(Query queryModel)
+        {
+            string query = System.IO.File.ReadAllText(S2_PATH);
+            query = query.Replace("X", "N\'" + queryModel.DevName + "\'");
+            query = query.Replace("\r\n", " ");
+            query = query.Replace('\t', ' ');
+
+            queryModel.QueryId = "S2";
+            queryModel.CustNames = new List<string>();
+            queryModel.CustSurnames = new List<string>();
+
+            using (var connection = new SqlConnection(Configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        int flag = 0;
+                        while (reader.Read())
+                        {
+                            queryModel.CustNames.Add(reader.GetString(0));
+                            queryModel.CustSurnames.Add(reader.GetString(1));
+                            flag++;
+                        }
+
+                        if (flag == 0)
+                        {
+                            queryModel.ErrorFlag = 1;
+                            queryModel.Error = Resourses.ERROR_CUSTOMERS;
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return RedirectToAction("Result", queryModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SimpleQuery3(Query queryModel)
+        {
+            string query = System.IO.File.ReadAllText(S3_PATH);
+            query = query.Replace("K", "N\'" + queryModel.CountryName + "\'");
+            query = query.Replace("\r\n", " ");
+            query = query.Replace('\t', ' ');
+
+            queryModel.QueryId = "S3";
+            queryModel.ProdNames= new List<string>();
+            queryModel.ProdPrices= new List<decimal>();
+
+            using (var connection = new SqlConnection(Configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        int flag = 0;
+                        while (reader.Read())
+                        {
+                            queryModel.ProdNames.Add(reader.GetString(0));
+                            queryModel.ProdPrices.Add(reader.GetDecimal(1));
+                            flag++;
+                        }
+
+                        if (flag == 0)
+                        {
+                            queryModel.ErrorFlag = 1;
+                            queryModel.Error = Resourses.ERROR_GAME_NOT_EXISTS;
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return RedirectToAction("Result", queryModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SimpleQuery4(Query queryModel)
+        {
+            string query = System.IO.File.ReadAllText(S4_PATH);
+            query = query.Replace("X", "N\'" + queryModel.CustName + "\'");
+            query = query.Replace("Y", "N\'" + queryModel.CustSurname + "\'");
+            query = query.Replace("Z", "N\'" + queryModel.CustEmail + "\'");
+            query = query.Replace("\r\n", " ");
+            query = query.Replace('\t', ' ');
+
+            queryModel.QueryId = "S4";
+            queryModel.DevNames = new List<string>();
+
+            using (var connection = new SqlConnection(Configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        int flag = 0;
+                        while (reader.Read())
+                        {
+                            queryModel.DevNames.Add(reader.GetString(0));
+                            flag++;
+                        }
+
+                        if (flag == 0)
+                        {
+                            queryModel.ErrorFlag = 1;
+                            queryModel.Error = Resourses.ERROR_DEV_NOT_EXISTS;
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return RedirectToAction("Result", queryModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SimpleQuery5(Query queryModel)
+        {
+            if (ModelState.IsValid)
+            {
+                string query = System.IO.File.ReadAllText(S5_PATH);
+                query = query.Replace("P", queryModel.Price.ToString());
+                query = query.Replace("\r\n", " ");
+                query = query.Replace('\t', ' ');
+
+                queryModel.QueryId = "S5";
+                queryModel.DevNames = new List<string>();
+
+                using (var connection = new SqlConnection(Configuration.GetConnectionString("DefaultConnection")))
+                {
+                    connection.Open();
+                    using (var command = new SqlCommand(query, connection))
+                    {
+                        command.ExecuteNonQuery();
+                        using (var reader = command.ExecuteReader())
+                        {
+                            int flag = 0;
+                            while (reader.Read())
+                            {
+                                queryModel.DevNames.Add(reader.GetString(0));
+                                flag++;
+                            }
+
+                            if (flag == 0)
+                            {
+                                queryModel.ErrorFlag = 1;
+                                queryModel.Error = Resourses.ERROR_DEV_NOT_EXISTS;
+                            }
+                        }
+                    }
+                    connection.Close();
+                }
+                return RedirectToAction("Result", queryModel);
+            }
+            return RedirectToAction("Index", new { errorCode = 1 });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SimpleQuery6(Query queryModel)
+        {
+            if (ModelState.IsValid)
+            {
+                string query = System.IO.File.ReadAllText(S6_PATH);
+                query = query.Replace("X", "N\'" + queryModel.ProdName + "\'");
+                query = query.Replace("\r\n", " ");
+                query = query.Replace('\t', ' ');
+                queryModel.QueryId = "S6";
+                queryModel.DevNames = new List<string>();
+
+                using (var connection = new SqlConnection(Configuration.GetConnectionString("DefaultConnection")))
+                {
+                    connection.Open();
+                    using (var command = new SqlCommand(query, connection))
+                    {
+                        command.ExecuteNonQuery();
+                        using (var reader = command.ExecuteReader())
+                        {
+                            int flag = 0;
+                            while (reader.Read())
+                            {
+                                queryModel.DevNames.Add(reader.GetString(0));
+                                flag++;
+                            }
+
+                            if (flag == 0)
+                            {
+                                queryModel.ErrorFlag = 1;
+                                queryModel.Error = Resourses.ERROR_DEV_NOT_EXISTS;
+                            }
+                        }
+                    }
+                    connection.Close();
+                }
+                return RedirectToAction("Result", queryModel);
+            }
+
+            return RedirectToAction("Index", new { errorCode = 2 });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AdvancedQuery1(Query queryModel)
+        {
+            string query = System.IO.File.ReadAllText(A1_PATH);
+            query = query.Replace("K", queryModel.DevId.ToString());
+            query = query.Replace("\r\n", " ");
+            query = query.Replace('\t', ' ');
+            queryModel.QueryId = "A1";
+            queryModel.CountryNames = new List<string>();
+
+            using (var connection = new SqlConnection(Configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        int flag = 0;
+                        while (reader.Read())
+                        {
+                            queryModel.CountryNames.Add(reader.GetString(0));
+                            flag++;
+                        }
+
+                        if (flag == 0)
+                        {
+                            queryModel.ErrorFlag = 1;
+                            queryModel.Error = Resourses.ERROR_COUNTRY_NOT_EXISTS;
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return RedirectToAction("Result", queryModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AdvancedQuery2(Query queryModel)
+        {
+            string query = System.IO.File.ReadAllText(A2_PATH);
+            query = query.Replace("Y", "N\'" + queryModel.CustEmail.ToString() + "\'");
+            query = query.Replace("\r\n", " ");
+            query = query.Replace('\t', ' ');
+            queryModel.QueryId = "A2";
+            queryModel.CustSurnames = new List<string>();
+
+            using (var connection = new SqlConnection(Configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        int flag = 0;
+                        while (reader.Read())
+                        {
+                            queryModel.CustSurnames.Add(reader.GetString(0));
+                            flag++;
+                        }
+
+                        if (flag == 0)
+                        {
+                            queryModel.ErrorFlag = 1;
+                            queryModel.Error = Resourses.ERROR_CUSTOMERS;
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return RedirectToAction("Result", queryModel);
+        }
+
+        public IActionResult AdvancedQuery3(Query queryModel)
+        {
+            string query = System.IO.File.ReadAllText(A3_PATH);
+            query = query.Replace("Y", "N\'" + queryModel.CustName.ToString() + "\'");
+            query = query.Replace("\r\n", " ");
+            query = query.Replace('\t', ' ');
+            queryModel.QueryId = "A3";
+            queryModel.CustSurnames = new List<string>();
+            queryModel.CustEmails = new List<string>();
+
+            using (var connection = new SqlConnection(Configuration.GetConnectionString("DefaultConnection")))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        int flag = 0;
+                        while (reader.Read())
+                        {
+                            queryModel.CustSurnames.Add(reader.GetString(0));
+                            queryModel.CustEmails.Add(reader.GetString(1));
+                            flag++;
+                        }
+
+                        if (flag == 0)
+                        {
+                            queryModel.ErrorFlag = 1;
+                            queryModel.Error = Resourses.ERROR_CUSTOMERS;
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            return RedirectToAction("Result", queryModel);
+        }
+
+        public IActionResult TeacherQuery1(Query queryModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult TeacherQuery2(Query queryModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Result(Query queryResult)
+        {
+            return View(queryResult);
+        }
+    }
+}
---
+++ Models/Resourses.cs
@@ -0,0 +1,29 @@
+﻿namespace TheGStore.Models
+{
+    public static class Resourses
+    {
+        #region ERRORS
+        public const string ERROR_IsEmpty = "Поле не може бути порожнім";
+        public const string ERROR_IncorectEmail = "Електронна адреси не коректна";
+        public const string ERROR_IncorectPrice = "Ціна не коректна";
+        public const string ERROR_GameExists = "Така гра вже існує";
+        public const string ERROR_DeveloperExists = "Такий розробник вже існує";
+        public const string ERROR_CountryExists = "Така країна вже існує";
+        public const string ERROR_GenreExists = "Такий жанр вже існує";
+        public const string ERROR_ALREADY_BOUGHT = "Ви вже придбали цей продукт";
+        #endregion ERRORS
+
+        #region Regular expresions
+        public const string REG_EXP_Email = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
+        public const string REG_EXP_Price = @"^[0-9]+(\.[0-9]{1,2})?";
+        #endregion Regular expresions
+
+        #region Query errors
+        public const string ERROR_AVG_PRICE = "Неможливо обрахувати середню ціну, оскільки продукти відсутні.";
+        public const string ERROR_CUSTOMERS = "Покупці, що задовольняють дану умову, відсутні.";
+        public const string ERROR_GAME_NOT_EXISTS = "Програмні продукти, що задовольняють дану умову, відсутні.";
+        public const string ERROR_DEV_NOT_EXISTS = "Розробники, що задовольняють дану умову, відсутні.";
+        public const string ERROR_COUNTRY_NOT_EXISTS = "Країни, що задовольняють дану умову, відсутні.";
+        #endregion Query errors
+    }
+}
